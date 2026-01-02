21169d7be73ddfd561b34877296579ad7e95d604
Muhammad Wildan Ferdiansyah <ferdiansyahwildan2@gmail.com>
2026-01-02 23:41:25 +0700

    feat: implement PUT /purchases API cancel purchases

--- backend/controller/purchase-controller.js
+++ backend/controller/purchase-controller.js
@@ -72,6 +72,74 @@ const createPurchase = (req, res) => {
   });
 };
 
+const cancelPurchase = (req, res) => {
+  const purchaseId = req.params.id;
+
+  const queryGetPurchase = `
+    SELECT product_id, quantity, status
+    FROM purchases
+    WHERE id = ?  
+  `;
+
+  db.query(queryGetPurchase, [purchaseId], (err, datas) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        message: "Failed to fetch purchase",
+      });
+    }
+
+    if (datas.length === 0) {
+      return res.status(404).json({
+        message: "Purchase not found",
+      });
+    }
+
+    const purchases = datas[0];
+
+    if (purchases.status === "CANCELLED") {
+      return res.status(400).json({
+        message: "Purchase already cancelled",
+      });
+    }
+
+    const queryUpdatePurchase = `
+      UPDATE purchases
+      SET status = 'CANCELLED'
+      WHERE id = ?
+    `;
+
+    db.query(queryUpdatePurchase, [purchaseId], (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({
+          message: "Failed to cancel purchase",
+        });
+      }
+
+      const queryRestockStock = `
+        UPDATE product_stocks
+        SET stock = stock + ?
+        WHERE product_id = ?
+      `;
+
+      db.query(queryRestockStock, [purchases.quantity, purchases.product_id], (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            message: "Failed to restock stock",
+          });
+        }
+
+        res.status(200).json({
+          message: "Purchase cancelled successfully",
+        })
+      });
+    })
+  });
+};
+
 module.exports = {
   createPurchase,
+  cancelPurchase,
 };
--- backend/routes/purchase-routes.js
+++ backend/routes/purchase-routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const purchaseController = require('../controller/purchase-controller');
 
 router.post('/', purchaseController.createPurchase);
+router.put('/:id/cancel', purchaseController.cancelPurchase);
 
 module.exports = router;
